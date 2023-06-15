65b099db419b8486342b4e6331d2c3c85f214ced
mmatczak <marek.matczak79@gmail.com>
2023-06-15 14:57:32 +0200

    Test cont.

--- src/app/book/components/book-details/book-details.component.spec.ts
+++ src/app/book/components/book-details/book-details.component.spec.ts
@@ -1,8 +1,10 @@
-import {fakeAsync, TestBed, tick} from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 import {BookDetailsComponent} from './book-details.component';
 import {Book} from '../../model';
 
 describe('BookDetailsComponent', function () {
+  const updatedAuthor = 'Updated Author';
+  const updatedTitle = 'Updated Title';
   let testBook: Book;
 
   beforeEach(() => {
@@ -16,7 +18,7 @@ describe('BookDetailsComponent', function () {
         preventDefault: jasmine.createSpy('preventDefault'),
         target: {
           querySelector(selector: string) {
-            const value = selector === '#author' ? 'Updated Author' : 'Updated Title'
+            const value = selector === '#author' ? updatedAuthor : updatedTitle
             return {value};
           }
         }
@@ -28,8 +30,8 @@ describe('BookDetailsComponent', function () {
         expect(eventStub.preventDefault).toHaveBeenCalled();
         expect(book).toBeTruthy();
         expect(book.id).toBe(testBook.id);
-        expect(book.author).toBe('Updated Author');
-        expect(book.title).toBe('Updated Title');
+        expect(book.author).toBe(updatedAuthor);
+        expect(book.title).toBe(updatedTitle);
         done();
       })
       // 2. when
@@ -38,14 +40,23 @@ describe('BookDetailsComponent', function () {
   });
 
   describe('(DOM)', function () {
-    it('populates input book data into form fields', function () {
-      // given
-      TestBed.configureTestingModule({
+    let fixture: ComponentFixture<BookDetailsComponent>;
+    let component: BookDetailsComponent;
+    let element: HTMLElement;
+
+    beforeEach(() => {
+      return TestBed.configureTestingModule({
         imports: [BookDetailsComponent]
-      });
-      const fixture = TestBed.createComponent(BookDetailsComponent);
-      const component = fixture.componentInstance;
-      const element = fixture.nativeElement as HTMLElement;
+      }).compileComponents();
+    })
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(BookDetailsComponent);
+      component = fixture.componentInstance;
+      element = fixture.nativeElement as HTMLElement;
+    });
+
+    it('populates input book data into form fields', function () {
       // when
       component.book = testBook;
       fixture.detectChanges();
@@ -55,8 +66,51 @@ describe('BookDetailsComponent', function () {
       expect(authorInput?.value).toBe(testBook.author);
     });
 
-    it('gets notified on book after clicking save button', function () {
-      // TODO :)
+    it('gets notified on book change after clicking save button', function () {
+      // 1. given
+      component.book = testBook;
+      fixture.detectChanges();
+      component.valueChange.subscribe(book => {
+        // 3. then
+        expect(book).toBeTruthy();
+        expect(book.id).toBe(testBook.id);
+        expect(book.author).toBe(updatedAuthor);
+        expect(book.title).toBe(updatedTitle);
+      })
+      // 2. when
+      createBookDetailsFrom(element)
+        .setAuthorInput(updatedAuthor)
+        .setTitleInput(updatedTitle)
+        .clickOnSave();
     })
   });
 });
+
+export function createBookDetailsFrom(element: HTMLElement) {
+  return {
+    setAuthorInput(newValue: string) {
+      getInputElementOf('#author').value = newValue;
+      return this;
+    },
+    setTitleInput(newValue: string) {
+      getInputElementOf('#title').value = newValue;
+      return this;
+    },
+    clickOnSave() {
+      const button = element.querySelector<HTMLButtonElement>('button');
+      if (!button) {
+        throw new Error('Button not found');
+      }
+      button.click();
+      return this;
+    }
+  }
+
+  function getInputElementOf(selector: string) {
+    const input = element.querySelector<HTMLInputElement>(selector);
+    if (!input) {
+      throw new Error(`No ${selector} input found!`);
+    }
+    return input;
+  }
+}
