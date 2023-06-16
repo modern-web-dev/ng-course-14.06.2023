94197654a421635189d3b19c16edd6e37bf4911c
Tomasz Wawrzyniak <tomasz.wawrzyniak@capgemini.com>
2023-06-16 12:12:35 +0200

    Simple Input

--- src/app/book/components/book-overview/book-overview.component.ts
+++ src/app/book/components/book-overview/book-overview.component.ts
@@ -3,26 +3,36 @@ import {CommonModule} from '@angular/common';
 import {BookDetailsComponent} from '../book-details/book-details.component';
 import {Book} from '../../model';
 import {BookService} from '../../services/book.service';
-import {Observable} from 'rxjs';
+import {debounceTime, filter, map, Observable, Subscription, switchMap} from 'rxjs';
 import {ActivatedRoute, Router} from '@angular/router';
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'ba-book-overview',
   standalone: true,
-  imports: [CommonModule, BookDetailsComponent],
+  imports: [CommonModule, BookDetailsComponent, ReactiveFormsModule],
   templateUrl: './book-overview.component.html',
   styleUrls: ['./book-overview.component.scss'],
 })
 export class BookOverviewComponent {
   readonly books$: Observable<Book[]>;
-  readonly typeaheadTest$: Observable<Book[]>;
+
+  typeaheadControl = new FormControl('default wartość', {nonNullable: true});
+  typeaheadBooks$: Observable<string[]>;
 
   constructor(private readonly books: BookService,
               private readonly router: Router,
               private readonly currentRoute: ActivatedRoute) {
     this.books$ = this.books.findAll();
-    this.typeaheadTest$ = this.books.search('to');
+    this.typeaheadBooks$ = this.typeaheadControl.valueChanges.pipe(
+      // filter((query) => query.length > 2),
+      debounceTime(500),
+      switchMap((query) => this.books.search(query)),
+      map((books: Book[]) => books.map(book => book.author))
+    )
+
   }
+
 
   goToDetailsOf(book: Book) {
     this.router.navigate([book.id], {relativeTo: this.currentRoute});
